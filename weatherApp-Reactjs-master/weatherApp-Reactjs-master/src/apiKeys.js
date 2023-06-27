c65cd777ed262e33debad12103b586c3ec19dfcc
Diyona10 <93899165+Diyona10@users.noreply.github.com>
2023-06-27 21:57:10 +0530

    Add files via upload

---
+++ weatherApp-Reactjs-master/weatherApp-Reactjs-master/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "0889993125f3d7de45a2a0aff8269b45",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
